07a82405a5e74cc54964da586b0b96e6103167dd
carolinasampognaTgT <41452195+carolinasampognaTgT@users.noreply.github.com>
2018-10-05 10:48:30 -0300

    2

--- src/test/java/Moura/core/LoginBaseTest.java
+++ src/test/java/Moura/core/LoginBaseTest.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.TakesScreenshot;
 import Moura.core.BaseLoginPage;
 import static Moura.core.DriverFactory.getDriver;
 //import static qa.cnabox.core.DriverFactory.killDriver;
+import static Moura.core.DriverFactory.getDriver;
 
 
 
@@ -32,5 +33,17 @@ public class LoginBaseTest {
 		
 		
 	}
+	@After
+	public void finaliza() throws IOException 
+	{
+		TakesScreenshot ss = (TakesScreenshot) getDriver();
+		File print = ss.getScreenshotAs(OutputType.FILE);
+
+		FileUtils.copyFile(print, new File("target" + File.separator + "screenshot" + File.separator
+				+ testName.getMethodName() +  ".jpg"));
+
+		
+		}
+	
 	
 }

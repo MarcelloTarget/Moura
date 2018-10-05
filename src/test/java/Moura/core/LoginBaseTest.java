22da46728c61395e95a8de0b50f100c59e98c4df
carolinasampognaTgT <41452195+carolinasampognaTgT@users.noreply.github.com>
2018-10-05 10:44:52 -0300

    1

--- src/test/java/Moura/core/LoginBaseTest.java
+++ src/test/java/Moura/core/LoginBaseTest.java
@@ -1,5 +1,36 @@
 package Moura.core;
 
+
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import Moura.core.BaseLoginPage;
+import static Moura.core.DriverFactory.getDriver;
+//import static qa.cnabox.core.DriverFactory.killDriver;
+
+
+
 public class LoginBaseTest {
 
+	@Rule
+	public TestName testName = new TestName();
+
+	private BaseLoginPage page = new BaseLoginPage();
+
+	@Before
+	public void inicializa() throws InterruptedException {
+		
+		page.acessarTelaInicial();
+		
+		
+		
+	}
+	
 }

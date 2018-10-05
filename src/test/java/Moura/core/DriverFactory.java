1e708f7b9cb685de298887e4e5b33b920858efc6
carolinasampognaTgT <41452195+carolinasampognaTgT@users.noreply.github.com>
2018-10-05 10:05:15 -0300

    Atualização

---
+++ src/test/java/Moura/core/DriverFactory.java
@@ -0,0 +1,47 @@
+package Moura.core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>() {
+		@Override
+		protected synchronized WebDriver initialValue() {
+			return initDriver();
+		}
+	};
+
+	private DriverFactory() {
+	}
+
+	public static WebDriver getDriver() {
+		return threadDriver.get();
+	}
+
+	public static WebDriver initDriver() {
+		WebDriver driver = null;
+		switch (Propriedades.browser) {
+		
+		case CHROME:
+			//System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+		}
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+	//public static void killDriver() {
+		//WebDriver driver = getDriver();
+		//if (driver != null) {
+			//driver.quit();
+			//driver = null;
+	//	}
+		//if (threadDriver != null) {
+			//threadDriver.remove();
+		//}
+	}
+
+//}
